295074ab6d26bcf25e3a96a561982993d79357a3
Buddha Lo <lo_977@yahoo.com>
2024-12-31 11:03:39 -0800

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,11 +1,22 @@
+# def write_file(file_name, file_content):
+#     with open(f"{file_name}.txt",'w')as f:
+#         f.write(file_content)
+
+# def append_file(file_name, append_content):
+#     with open(f"{file_name}.txt",'a')as f:
+#         f.write(append_content)
+
+# def read_file(file_name):
+#     with open(f'{file_name}.txt') as f:
+#         return f.read()
 def write_file(file_name, file_content):
-    with open(f"{file_name}.txt",'w')as f:
-        f.write(file_content)
+    with open(f"{file_name}.txt",mode='w', encoding='utf-8')as write_file:
+        write_file.write(file_content)
 
 def append_file(file_name, append_content):
-    with open(f"{file_name}.txt",'a')as f:
-        f.write(append_content)
+    with open(f"{file_name}.txt",mode='a', encoding='utf-8')as append_file:
+        append_file.write(append_content)
 
 def read_file(file_name):
-    with open(f'{file_name}.txt') as f:
-        return f.read()
+    with open(f'{file_name}.txt',mode="r", encoding='utf-8') as read_file:
+        return read_file.read()
